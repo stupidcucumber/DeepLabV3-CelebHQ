6685ac5dbb9c7fdbeb5a3593604f4c654ed18e57
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-02-17 16:23:39 +0200

    Created function for instantiating model

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import logging
 import logging.config
 import pathlib
 import json
+from torchvision import models
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+import argparse
 
 logger = logging.getLogger('training_logger')
 
@@ -16,7 +19,24 @@ def setup_logging():
     logging.config.dictConfig(config=config)
 
 
+def parse_configs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output-channels', type=int, default=18)
+    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--batch-size', type=int, default=32)
+    args = parser.parse_args()
+    logger.info('Parsed arguments.', extra={key: value for key, value in args._get_kwargs()})
+    return args
+
+
+def create_model(output_channels: int = 1):
+    model = models.segmentation.deeplabv3_resnet101(pretrained=True, progrss=True)
+    model.classifier = DeepLabHead(2048, output_channels)
+    logger.info('Instantiated model.')
+    return model
+
+
 if __name__ == '__main__':
     setup_logging()
-    
-    logger.info('I want to break free!')
+    args = parse_configs()
+    model = create_model(output_channels=args.output_channels)
