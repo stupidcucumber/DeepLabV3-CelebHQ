c796a05001092e9f5eece418f06b6c96b31264a6
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-02-18 22:07:11 +0200

    Implemented accuracy evaluator

--- src/evaluators/accuracy.py
+++ src/evaluators/accuracy.py
@@ -7,16 +7,25 @@ from ..utils import decode
 class AccuracyMeanEvaluator(MeanEvaluator):
     def __init__(self, name: str):
         super(AccuracyMeanEvaluator, self).__init__(name=name)
+    
+    def _calculate_accuracy_layer(self, layer_predicted, layer_true) -> np.float32:
+        _mp = np.multiply(layer_predicted, layer_true)
+        intersection = np.sum(_mp)
+        union = np.sum(np.asarray([layer_predicted, layer_true])) - intersection
+        accuracy = intersection / union
+        return accuracy
 
-    def calculate_value(self, logits: Tensor, labels: Tensor) -> np.float32:
+    def calculate_value(self, logits: Tensor, labels: Tensor) -> dict | np.float32:
         _batch_num, _layer_num = logits.shape[:2]
         accuracies = np.zeros(shape=(_layer_num, _batch_num))
         for batch_index, (sample_logits, sample_label) in enumerate(zip(logits, labels)):
             decoded_logits = decode(sample_logits, dim=0)
             for layer_index, (predicted_layer, true_layer) in enumerate(zip(decoded_logits, sample_label)):
-                _mp = np.multiply(predicted_layer, true_layer)
-                intersection = np.sum(_mp)
-                union = np.sum(np.asarray([predicted_layer, true_layer])) - intersection
-                _accuracy = intersection / union
-                accuracies[layer_index, batch_index] = _accuracy
-        return accuracies.mean(axis=1)
+                accuracies[layer_index, batch_index] \
+                    = self._calculate_accuracy_layer(layer_predicted=predicted_layer, layer_true=true_layer)
+        if self.mapping:
+            result_accuracies = accuracies.mean(axis=1)
+            result = {self.mapping[index]: result_accuracies[index] for index in range(_layer_num)}
+        else:
+            result = accuracies.mean(axis=0)
+        return result
--- src/evaluators/evaluator.py
+++ src/evaluators/evaluator.py
@@ -4,29 +4,64 @@ import numpy as np
 
 class MeanEvaluator:
     '''
-        
+        This is the abstract class for the evaluators.
     '''
-    def __init__(self, name: str):
+    def __init__(self, name: str, mapping: dict | None = None):
+        '''
+            :param name: The name of the evaluator
+            :param mapping: The mapping name for layer, it comes as a list of dicts with keys - index: name. E.g.: {0: 'background', ...},
+                the number of dicts must be the same as the size of the first non-batch dimention.
+            :return: Returns nothing.
+        '''
         super(MeanEvaluator, self).__init__()
         self.name = name
-        self.values = []
+        self.mapping = mapping
+        if self.mapping:
+            self.values = {key: [] for key in self.mapping.values()}
+        else:
+            self.values = []
         self.result = None
 
-    def get_result(self) -> np.float32:
+    def get_result(self) -> dict[str, np.float32] | np.float32:
         if self.result is None:
-            self.result = np.mean(self.values)
+            if self.mapping:
+                self.result = {key: np.mean(value) for key, value in self.values.items()}
+            else:
+                self.result = np.mean(self.values)
         return self.result
 
-    def calculate_value(self, logits: torch.Tensor, labels: torch.Tensor) -> np.float32:
+    def calculate_value(self, logits: torch.Tensor, labels: torch.Tensor) -> dict[str, np.float32] | np.float32:
+        '''
+            Function that calculates value of the metrics. If dict mapping is being passed then the result returned is in format
+        {'layer_name': metric, ...}. This is only virtual function and therefore must be defined in the derived class.
+        
+        :param logits: Logits of the model.
+        :param labels:  True Labels of the model.
+        :return: Either dict[str, np.float32] or np.float32
+        '''
         raise NotImplementedError('This method must be implemented in the derived class.')
 
     def append(self, logits: torch.Tensor, labels: torch.Tensor):
-        value = self.calculate_value(logits=logits, labels=labels)
-        self.values.append(value)
+        if self.mapping:
+            _dict = self.calculate_value(logits=logits, labels=labels)
+            for key in self.values.keys():
+                self.values[key].append(_dict[key])
+        else:
+            value = self.calculate_value(logits=logits, labels=labels)
+            self.values.append(value)
         self.result = None
 
     def zero(self):
-        self.values.clear()
+        '''
+            This function zeros the calculator, so it can calculate new values on the new epoch.
+        '''
+        if self.mapping:
+            for key in self.mapping.keys():
+                self.values[key].clear()
+        else:
+            self.values.clear()
 
     def __getitem__(self, index) -> np.float32:
+        if self.mapping:
+            return {key: value[index] for key, value in self.values.items}
         return self.values[index]
