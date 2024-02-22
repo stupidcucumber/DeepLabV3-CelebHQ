6c36849108b8d270d0b19752d3406c404451e2d1
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-02-22 11:05:30 +0200

    Added callbacks calls in the trainer code

--- src/trainer.py
+++ src/trainer.py
@@ -3,6 +3,7 @@ from torch.nn import Module
 import torch
 import logging
 from .evaluators import MeanEvaluator
+from .callback import Callback
 
 
 class Trainer:
@@ -10,12 +11,14 @@ class Trainer:
                  loss_fn,
                  optimizer: torch.optim.Optimizer,
                  logger: logging.Logger,
-                 evaluators: list[MeanEvaluator]):
+                 evaluators: list[MeanEvaluator],
+                 callbacks: list[Callback]):
         self.model = model
         self.logger = logger
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.evaluators = evaluators
+        self.callbacks = callbacks
 
     def train_step(self, logits, labels) -> float:
         self.model.train()
@@ -33,9 +36,19 @@ class Trainer:
     def fit(self, train_loader: DataLoader, val_loader: DataLoader,
                  epochs: int):
         self.logger.info('Start fitting the model.')
-
+        data = {
+            'model': self.model,
+            'train_loss': 0,
+            'val_loss': 0,
+            'extra_train': dict(),
+            'extra_val' : dict()
+        }
         for epoch in range(epochs):
             losses = []
+
+            for callback in self.callbacks:
+                callback.epoch_start(data=data)
+
             for inputs, labels in train_loader:
                 logits = self.model(inputs)['out'].float()
                 loss = self.train_step(logits=logits, labels=labels)
@@ -45,7 +58,9 @@ class Trainer:
                 for evaluator in self.evaluators:
                     evaluator.append(logits=logits, labels=labels)
                     print('Results of %s' % evaluator.name, evaluator.get_result())
+                    data['extra_train'][evaluator.name] = evaluator.get_result()
             self.logger.info('training', extra={'epoch': epoch, 'average_loss': average_loss})
+            data['train_loss'] = average_loss
 
             losses.clear()
             with torch.no_grad():
@@ -58,6 +73,11 @@ class Trainer:
                     for evaluator in self.evaluators:
                         evaluator.append(logits=logits, labels=labels)
                         print('Results of %s' % evaluator.name, evaluator.get_result())
+                        data['extra_val'][evaluator.name] = evaluator.get_result()
                 self.logger.info('validating', extra={'epoch': epoch, 'average_loss': average_loss})
+                data['val_loss'] = average_loss
+            
+            for callback in self.callbacks:
+                callback.epoch_end(data=data)
 
         self.logger.info('Fitting has been ended.')
