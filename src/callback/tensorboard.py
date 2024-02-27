b62802047d6c9c5e5b828b9787382f4cdd672dad
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-02-27 19:16:15 +0200

    Code refactoring

--- src/callback/tensorboard.py
+++ src/callback/tensorboard.py
@@ -1,10 +1,13 @@
 from typing import Any
+import pathlib
+from torch.utils.tensorboard import SummaryWriter
 from .base import Callback
 
 
 class TensorboardCallback(Callback):
-    def __init__(self) -> None:
+    def __init__(self, log_dir: str | pathlib.Path = 'logs') -> None:
         super(TensorboardCallback, self).__init__()
+        self.writer = SummaryWriter(log_dir=log_dir)
 
     def epoch_end(self, data: Any) -> None:
         return super().epoch_end(data)
--- src/trainer.py
+++ src/trainer.py
@@ -24,7 +24,7 @@ class Trainer:
     def _move_to_device(self, inputs: torch.Tensor, labels: torch.Tensor) -> list[torch.Tensor, torch.Tensor]:
         return inputs.to(self.device), labels.to(self.device)
 
-    def train_step(self, logits, labels) -> float:
+    def _train_step(self, logits, labels) -> float:
         self.model.train()
         self.optimizer.zero_grad()
         loss_1 = self.loss_fn(logits['out'], labels)
@@ -34,12 +34,32 @@ class Trainer:
         self.optimizer.step()
         return loss
 
-    def val_step(self, logits, labels) -> float:
+    def _val_step(self, logits, labels) -> float:
         self.model.eval()
         loss_1 = self.loss_fn(logits['out'], labels)
         loss_2 = self.loss_fn(logits['aux'], labels)
         loss = loss_1 + loss_2
         return loss
+    
+    def _evaluate(self, data: dict, logits: torch.Tensor, labels: torch.Tensor, partition: str = 'train'):
+        for evaluator in self.evaluators:
+            evaluator.append(logits=logits, labels=labels)
+            print('Results of %s' % evaluator.name, evaluator.get_result(), flush=True)
+            data['extra_%s' % partition][evaluator.name] = evaluator.get_result()
+    
+    def _compute_epoch(self, data: dict, loader: DataLoader, partition: str = 'train'):
+        losses = []
+        for inputs, labels in loader:
+            inputs, labels = self._move_to_device(inputs=inputs, labels=labels)
+            output = self.model(inputs)
+            loss = self._train_step(logits=output, labels=labels) if partition == 'train' \
+                else self._val_step(logits=output, labels=labels)
+            logits = output['out'].float()
+            losses.append(loss)
+            average_loss = torch.mean(torch.as_tensor(losses, dtype=torch.float32))
+            print('Loss is: ', average_loss, flush=True)
+            self._evaluate(data=data, logits=logits, labels=labels, partition=partition)
+        return average_loss
 
     def fit(self, train_loader: DataLoader, val_loader: DataLoader,
                  epochs: int):
@@ -56,36 +76,14 @@ class Trainer:
 
             for callback in self.callbacks:
                 callback.epoch_start(data=data)
-
-            for inputs, labels in train_loader:
-                inputs, labels = self._move_to_device(inputs=inputs, labels=labels)
-                output = self.model(inputs)
-                loss = self.train_step(logits=output, labels=labels)
-                logits = output['out'].float()
-                losses.append(loss)
-                average_loss = torch.mean(torch.as_tensor(losses, dtype=torch.float32))
-                print('Loss is: ', average_loss, flush=True)
-                for evaluator in self.evaluators:
-                    evaluator.append(logits=logits, labels=labels)
-                    print('Results of %s' % evaluator.name, evaluator.get_result(), flush=True)
-                    data['extra_train'][evaluator.name] = evaluator.get_result()
+            
+            average_loss = self._compute_epoch(data=data, loader=train_loader, partition='train')
             logger.info('training', extra={'epoch': epoch, 'average_loss': average_loss})
             data['train_loss'] = average_loss
 
             losses.clear()
             with torch.no_grad():
-                for inputs, labels in val_loader:
-                    inputs, labels = self._move_to_device(inputs=inputs, labels=labels)
-                    output = self.model(inputs)['out'].float()
-                    loss = self.val_step(logits=output, labels=labels)
-                    logits = output['out'].float()
-                    losses.append(loss)
-                    average_loss = torch.mean(torch.as_tensor(losses, dtype=torch.float32))
-                    print('Loss is: ', average_loss, flush=True)
-                    for evaluator in self.evaluators:
-                        evaluator.append(logits=logits, labels=labels)
-                        print('Results of %s' % evaluator.name, evaluator.get_result(), flush=True)
-                        data['extra_val'][evaluator.name] = evaluator.get_result()
+                average_loss = self._compute_epoch(data=data, loader=val_loader, partition='val')
                 logger.info('validating', extra={'epoch': epoch, 'average_loss': average_loss})
                 data['val_loss'] = average_loss
             
